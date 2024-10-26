9a1ee9ac7a327accd0787d04374dee5ef4818caf
Neelesh Nainwal <neeleshnainwal2001@gmail.com>
2024-10-26 14:37:27 +0530

    project

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Footer, Navbar, Product } from "../components"
+import { Navbar, Product } from "../components"
 
 const Products = () => {
   return (
     <>
       <Navbar />
       <Product />
-      <Footer />
+     
     </>
   )
 }
